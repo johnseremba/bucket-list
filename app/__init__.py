da102065c16fb6c374b816dc913221fb25560ddf
Seremba John <john.seremba@andela.com>
2017-07-07 14:46:54 +0300

    Add get authentication token

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 import os
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
+from functools import wraps
 
+from flask import Flask, session, flash, url_for
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.utils import redirect
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
@@ -10,5 +12,26 @@ app.config['WTF_CSRF_ENABLED'] = True
 
 # Database configuration
 app.config['SECRETE_KEY'] = 'eBL0S*wmc2mg?.&;R-7=Z@J+fI)4=QQYu`:}qMA#e>3<R9"[VQ<>%b!1?J!jwD,'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'bucket_list_db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////temp/bucketlist_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
+
+# User authentication
+login_manager = LoginManager()
+login_manager.session_protection = "strong"
+login_manager.init_app(app)
+
+from app.views import *
+from app.models import *
+
+
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash("You need to login first")
+            return redirect(url_for('login_page'))
+
+    return wrap
--- app/views.py
+++ app/views.py
@@ -1,5 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, g
 from flask_sqlalchemy import SQLAlchemy
+from requests import auth
+
+# from app import login_required
+from app.forms import LoginForm
+from app.models import User
 
 
 def create_app():
@@ -16,6 +21,31 @@ def index():
     # db.session.commit()
     return render_template('index.html')
 
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    return render_template('login.html', form=form)
+
+
+@app.route('/api/token')
+# @login_required
+def get_auth_token():
+    token = g.user.generate_auth_token()
+    return jsonify({ 'token': token.decode('ascii') })
+
+
+# @auth.verify_password
+def verify_password(username_or_token, password):
+    # first try to authenticate by token
+    user = User.verify_auth_token(username_or_token)
+    if not user:
+        # try to authenticate with username/password
+        user = User.query.filter_by(username = username_or_token).first()
+        if not user or not user.verify_password(password):
+            return False
+    g.user = user
+    return True
 
 if __name__ == "__main__":
     app.run(debug=True)
