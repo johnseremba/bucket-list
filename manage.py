8a88c309cfccd0c34db52524f7ef9e9ce57f7eea
Seremba John <john.seremba@andela.com>
2017-07-18 21:02:07 +0300

    Add Configuration Tests add Stagging configuration

--- manage.py
+++ manage.py
@@ -1,9 +1,6 @@
-import unittest
-
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager, prompt_bool
 from app import create_app, db
-from app.api.tests import test_create_user
 
 
 app = create_app('development')
@@ -23,12 +20,6 @@ def drop_db():
     if prompt_bool("Are you sure you want to delete the database?"):
         db.drop_all()
         print("Database dropped!")
-
-
-@manager.command
-def test():
-    suite = unittest.defaultTestLoader.loadTestsFromTestCase(test_create_user.FlaskTestCase)
-    unittest.TextTestRunner().run(suite)
 
 
 if __name__ == "__main__":
